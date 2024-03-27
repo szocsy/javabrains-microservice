786c815ebc71ca2a2c072591a51fb90886b00615
szocsy <szocsy@gmail.com>
2024-03-27 23:16:05 +0100

    JavaBrains Microservice learning part 1-12

---
+++ movie-catalog-service/src/main/java/com/szocsy/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,38 @@
+package com.szocsy.moviecatalogservice.resources;
+
+import com.szocsy.moviecatalogservice.models.CatalogItem;
+import com.szocsy.moviecatalogservice.models.Movie;
+import com.szocsy.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    RestTemplate restTemplate = new RestTemplate();
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+
+        return ratings.stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "description", rating.getRating());
+                })
+                .collect(Collectors.toList());
+    }
+}
