f541738af6d24a152d76009adf9c05f309190e0b
szocsy <szocsy@gmail.com>
2024-03-29 20:25:25 +0100

    13 webflux

--- movie-catalog-service/src/main/java/com/szocsy/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/szocsy/moviecatalogservice/resources/MovieCatalogResource.java
@@ -4,10 +4,12 @@ import com.szocsy.moviecatalogservice.models.CatalogItem;
 import com.szocsy.moviecatalogservice.models.Movie;
 import com.szocsy.moviecatalogservice.models.Rating;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -19,7 +21,7 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
 
     @Autowired
-    RestTemplate restTemplate = new RestTemplate();
+    private WebClient.Builder builder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -30,7 +32,8 @@ public class MovieCatalogResource {
         );
 
         return ratings.stream().map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    Movie movie = builder.build().get().uri("http://localhost:8082/movies/" + rating.getMovieId()).retrieve().bodyToMono(Movie.class).block();
                     return new CatalogItem(movie.getName(), "description", rating.getRating());
                 })
                 .collect(Collectors.toList());
